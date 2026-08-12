7b713d41ff50a8557e02aa09836ffcdf4c209a66
Segundo Alvarez <segundoav@hotmail.com>
2026-08-12 15:14:08 -0500

    avance

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/WDD330-sleepoutside-w03/",
+  //base: "/WDD330-sleepoutside-w03/",
   root: "src/",
   build: {
     outDir: "../dist",
